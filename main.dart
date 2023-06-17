a5cd993adb6b2ff821e7a271fbf6138a48350ddd
MazharMuzaffer <mazharq45@gmail.com>
2023-06-17 20:32:24 +0500

    assignment

---
+++ main.dart
@@ -0,0 +1,35 @@
+void main (){
+
+int length = 12;
+int breadth = 16;
+if (length == breadth) {
+print('this is sqaure')
+}else {
+  print('this is rectengle')
+}
+
+
+// Q2
+  int age1=25;
+  int age2=55; 
+
+ if (age1>age2){
+  print('oldest');} 
+ else {
+    print ('youngest'); }
+  
+}
+
+// Q3
+int classAttend = 10;
+  int classHeld = 16;
+  double studentAttendencePercentage = classAttend / classHeld * 100;
+  if (studentAttendencePercentage > 75) {
+    print(
+        'student is allow to sit in class and his percentage is $studentAttendencePercentage and he attended the class $classAttend');
+  } else {
+    print(
+        'student is not allow to sit in class and his percentage is $studentAttendencePercentage and he attended the class $classAttend');
+  }
+
+}
